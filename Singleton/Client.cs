bcbbe226f01b2b3f90b159f7811f1f81d5d407fb
HrMilev <43407775+HrMilev@users.noreply.github.com>
2019-02-09 22:00:17 +0200

    Add files via upload

---
+++ Singleton/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Singleton
+{
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Singleton first = Singleton.Instance;
+            Singleton second = Singleton.Instance;
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Lazy<Singleton> instance = new Lazy<Singleton>(()=> new Singleton());
+
+        Singleton() { }
+
+        public static Singleton Instance => instance.Value;
+
+    }
+}
