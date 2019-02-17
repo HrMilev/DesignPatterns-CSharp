7ee7e031cb7609121aeeae45e31ab7710d69c171
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-02-18 00:24:32 +0200

    Add files via upload

---
+++ Proxy/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client 
+{
+    static void Main(string[] args)
+    {
+        ISubject subject = new SubjectAccessor.Proxy();
+        Console.WriteLine(subject.Request());
+        Console.WriteLine(subject.Request());
+
+        subject = new SubjectAccessor.ProtectionProxy();
+        Console.WriteLine(subject.Request());
+        Console.WriteLine((subject as SubjectAccessor.ProtectionProxy).Authenticate("gru"));
+        Console.WriteLine((subject as SubjectAccessor.ProtectionProxy).Authenticate("bla"));
+        Console.WriteLine(subject.Request());
+
+    }
+}
