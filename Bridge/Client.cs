557cf27e170c590ba2976cf65eddff1ae9dfc58b
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-02-20 23:32:27 +0200

    Add files via upload

---
+++ Bridge/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Client
+{
+    static void Main()
+    {
+        Abstraction abstractBridge = new Abstraction(new ImpA());
+        Console.WriteLine(abstractBridge.Operation());
+        abstractBridge = new Abstraction(new ImpB());
+        Console.WriteLine(abstractBridge.Operation());
+
+    }
+}
