c2162536acbd321cf6d4cdbe869ab9e2ee839dcb
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-02-25 21:58:27 +0200

    Add files via upload

---
+++ Composite/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        IComponent<string> pictureFirst = new Component<string>("Me on the Beach");
+        IComponent<string> pictureSecond = new Component<string>("Me on the Mountin");
+        IComponent<string> pictureThird = new Component<string>("Me on the Tree");
+
+        IComponent<string> album = new Composite<string>("Dream album");
+
+        album.Add(pictureFirst);
+        album.Add(pictureSecond);
+        album.Add(pictureThird);
+
+        Console.WriteLine(album.Display(5));
+
+        album.Remove("Me on the Beach");
+
+        Console.WriteLine();
+
+        Console.WriteLine(album.Display(5));
+
+    }
+}
---
+++ Composite/Component.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Component<T> : IComponent<T>
+{
+    public T Item { get; set; }
+
+    public Component(T item)
+    {
+        Item = item;
+    }
+
+    public void Add(IComponent<T> c)
+    {
+        Console.WriteLine("Cannot add an item");
+    }
+
+    public string Display(int depth)
+    {
+        return new String('-', depth) + Item + "\n";
+    }
+
+    public IComponent<T> Find(T s)
+    {
+        if (s.Equals(Item))
+        {
+            return this;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public IComponent<T> Remove(T s)
+    {
+        Console.WriteLine("Cannot remove directly");
+        return this;
+    }
+
+
+}
+
