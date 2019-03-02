044475c923f60f0e6074effcd461de8b3266c01b
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-02 17:45:46 +0200

    Add files via upload

---
+++ FactoryMethod/Baker.cs
@@ -0,0 +1,14 @@
+﻿class Baker
+{
+    public ICake MakeCakeFactoryMethod(int time)
+    {
+        if (time < 8 && time > 0)
+        {
+            return new ValinaCake();
+        }
+        else
+        {
+            return new ChocolateCake();
+        }
+    }
+}
---
+++ FactoryMethod/ChocolateCake.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ChocolateCake : ICake
+{
+    public void Type()
+    {
+        Console.WriteLine("I am chocolateeeeeeee.....");
+    }
+}
