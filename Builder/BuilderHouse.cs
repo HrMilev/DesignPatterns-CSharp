7dd98a4e7804cdb18947e6534f480a2d92981e10
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-10 20:58:15 +0200

    Add files via upload

---
+++ Builder/BuilderHouse.cs
@@ -0,0 +1,24 @@
+﻿public class BuilderHouse : IBuilder
+{
+    House house = new House();
+
+    public void AddRoof()
+    {
+        this.house.Add("I have ... no roof");
+    }
+
+    public void AddDoor()
+    {
+        this.house.Add("I have ... curtains");
+    }
+
+    public void AddWindow()
+    {
+        this.house.Add("I have windows at least");
+    }
+
+    public House GetHouse()
+    {
+        return house;
+    }
+}
---
+++ Builder/BuilderLuxHouse.cs
@@ -0,0 +1,24 @@
+﻿public class BuilderLuxHouse : IBuilder
+{
+    House house = new House();
+
+    public void AddRoof()
+    {
+        this.house.Add("I have roof with tiles");
+    }
+
+    public void AddDoor()
+    {
+        this.house.Add("I have front door");
+    }
+
+    public void AddWindow()
+    {
+        this.house.Add("I have windows on the roof");
+    }
+
+    public House GetHouse()
+    {
+        return house;
+    }
+}
