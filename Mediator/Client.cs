9bc2ac1d400865026a681b9e0b53667a39e883f1
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-21 23:53:10 +0200

    Add files via upload

---
+++ Mediator/Client.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Multiphone phone = new Multiphone();
+
+        Friend bestFirst = new Friend(phone, "John");
+        Friend bestSecond = new Friend(phone, "Jill");
+        Friend bestThird = new Friend(phone, "Gillig");
+
+        bestFirst.MakeCall("How are you?");
+    }
+}
