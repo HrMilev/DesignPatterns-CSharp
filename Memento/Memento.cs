b7973d77132e761cbfd8173b0f84058715a885c4
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-23 00:25:02 +0200

    Add files via upload

---
+++ Memento/Memento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class Memento
+{
+    MemoryStream stream = new MemoryStream();
+    BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+    public Memento Save(object o)
+    {
+        binaryFormatter.Serialize(stream, o);
+        return this;
+    }
+
+    public object Restore()
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        object o = binaryFormatter.Deserialize(stream);
+        stream.Close();
+        return o;
+    }
+}
