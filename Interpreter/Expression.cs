82dc06f7c873cf87238cfe36d947e57e63540624
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-23 21:54:16 +0200

    Add files via upload

---
+++ Interpreter/Expression.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+class Expression
+{
+    private delegate double Operation(double a, double b);
+
+    private double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    private double Subtraction(double a, double b)
+    {
+        return a - b;
+    }
+
+    private Operation Decision(char c)
+    {
+        if (c == '+')
+        {
+            return Add;
+        }
+        else if (c == '-')
+        {
+            return Subtraction;
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+
+    public void Interpret(Content content)
+    {
+        content.Output += content.Input.First() - '0';
+        content.Input = content.Input.Substring(1);
+        while (content.Input != "")
+        {
+            Operation currOperation = Decision(content.Input.First());
+            content.Input = content.Input.Substring(1);
+            double currentNumber = content.Input.First() - '0';
+            content.Input = content.Input.Substring(1);
+            content.Output = currOperation(content.Output, currentNumber);
+        }
+    }
+}
+
