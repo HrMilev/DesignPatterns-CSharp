e8de34053eecefb42c650b32ae30158449e48d7e
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-24 19:01:23 +0200

    Add files via upload

---
+++ Observer/Observer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Observer : IObserver
+{
+    private string name;
+
+    public Observer(string name)
+    {
+        this.name = name;
+    }
+
+    public void ReceivePost(string name, string text)
+    {
+        Console.WriteLine($"{this.name} recieve post from {name}: \n {text}");
+    }
+}
