d7b5db7e1f4e0ff978c3bc14d8470e01dc558d3c
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-27 23:34:54 +0200

    Add files via upload

---
+++ Strategy/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input your magic number:");
+        DummyCalculator dummy = new DummyCalculator(double.Parse(Console.ReadLine()));
+        Console.Write("Input your enchanting operation symbol:");
+        string operation = Console.ReadLine();
+        while (!operation.Equals("="))
+        {
+            switch (operation)
+            {
+                case "*":
+                    dummy.ChooseOperation(new Multiply(dummy.InternalNumber));
+                    break;
+                case "/":
+                    dummy.ChooseOperation(new Divide(dummy.InternalNumber));
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, please try harder:");
+                    operation = Console.ReadLine();
+                    continue;
+            }
+            Console.Write("Input your next number:");
+            dummy.By(double.Parse(Console.ReadLine()));
+            Console.Write("Input your enchanting operation symbol:");
+            operation = Console.ReadLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("Voalaaa: " + dummy.InternalNumber);
+    }
+}
---
+++ Strategy/Divide.cs
@@ -0,0 +1,12 @@
+﻿public class Divide : IOperation
+{
+    double internalNumber;
+    public Divide(double internalNumber)
+    {
+        this.internalNumber = internalNumber;
+    }
+    public double By(double number)
+    {
+        return internalNumber / number;
+    }
+}
