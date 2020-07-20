197ae92141a4537946b3c89bcbbac85bd7555899
HrMilev <hr.milev@yahoo.bg>
2020-07-20 22:58:44 +0300

    Init Strategy

--- Strategy/Client.cs
+++ Strategy/Client.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-class Client
+namespace Strategy
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Write("Input your magic number:");
-        DummyCalculator dummy = new DummyCalculator(double.Parse(Console.ReadLine()));
-        Console.Write("Input your enchanting operation symbol:");
-        string operation = Console.ReadLine();
-        while (!operation.Equals("="))
+        static void Main(string[] args)
         {
-            switch (operation)
-            {
-                case "*":
-                    dummy.ChooseOperation(new Multiply(dummy.InternalNumber));
-                    break;
-                case "/":
-                    dummy.ChooseOperation(new Divide(dummy.InternalNumber));
-                    break;
-                default:
-                    Console.WriteLine("Invalid input, please try harder:");
-                    operation = Console.ReadLine();
-                    continue;
-            }
-            Console.Write("Input your next number:");
-            dummy.By(double.Parse(Console.ReadLine()));
-            Console.Write("Input your enchanting operation symbol:");
-            operation = Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
-        Console.WriteLine();
-        Console.WriteLine("Voalaaa: " + dummy.InternalNumber);
     }
 }
---
+++ TemplateMethod/README.md
@@ -0,0 +1,5 @@
+#Template Method Pattern
+
+Provides a skeleton in base class for a set of algorithms with different implementation parts.
+It allows subclasses to override specific steps of the algorithm without changing its structure.
+
